eb711fbf48f203c409e3515ebe0d248d0f5cc54d
Ayse Stinnett <ayse.stinnett@gmail.com>
2024-09-10 20:42:19 -0400

    day 8 prep

--- public/Day_06/task01.ts
+++ public/Day_06/task01.ts
@@ -4,26 +4,26 @@
  * You can add guests at the beginning, middle, and end of an array.
  */
 
-const guest: string[] = ["Hamza", "Haris", "Mudassir"];
+const guestArray: string[] = ["Hamza", "Haris", "Mudassir"];
 
-const personNotComing: number = guest.indexOf("Mudassir");
-const replacementGuest: string = "Taha";
+const personNotComingSix: number = guestArray.indexOf("Mudassir");
+const replacementGuestSix: string = "Taha";
 
-if (personNotComing !== -1) {
-  guest[personNotComing] = replacementGuest;
+if (personNotComingSix !== -1) {
+  guestArray[personNotComingSix] = replacementGuestSix;
 }
 
-for (let i = 0; i < guest.length; i++) {
-  console.log(`Hello ${guest[i]}, you are invited to the dinner`);
+for (let i = 0; i < guestArray.length; i++) {
+  console.log(`Hello ${guestArray[i]}, you are invited to the dinner`);
 }
 
 console.log("\nGreat news! I found a bigger dinner table! \n");
 
-// Lets add more guests
-guest.unshift("Faisal"); // add guest at beginning
-guest.splice(guest.length / 2, 0, "Fahad"); // add guest at middle
-guest.push("Bilal"); // add guest at end
+// Lets add more guestArrays
+guestArray.unshift("Faisal"); // add guestArray at beginning
+guestArray.splice(guestArray.length / 2, 0, "Fahad"); // add guestArray at middle
+guestArray.push("Bilal"); // add guestArray at end
 
-for (let i = 0; i < guest.length; i++) {
-  console.log(`Hello ${guest[i]}, you are invited to the dinner`);
+for (let i = 0; i < guestArray.length; i++) {
+  console.log(`Hello ${guestArray[i]}, you are invited to the dinner`);
 }
